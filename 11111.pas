eda02bbf2a9c8d2db7c92e66e7e53a917ddbccc5
Jekaexe <117809005+Jekaexe@users.noreply.github.com>
2022-11-09 11:13:53 +0300

    Add files via upload

---
+++ 11111.pas
@@ -0,0 +1,21 @@
+﻿var x:real;
+begin
+writeln ('Введите х');
+readln(x);
+if x<-8 then 
+writeln('1 формула x = ',(cos(2*x)*((-1)*x+x**3)/cos(x)):0:2)
+else 
+writeln('1 функция не подходит по введенным значениям');
+if (-8<=x) and (x<-5) then 
+writeln('2 формула x = ',59+ln(x):0:2)
+else 
+writeln('2 функция не подходит по введенным значениям');
+if (-5<=x) and (x<6) then 
+writeln('3 формула x = ',(-1)*x + tan(x):0:2)
+else 
+writeln('3 функция не подходит по введенным значениям');
+if 6<=x then
+writeln('4 формула x = ',ln(x) * ln(x)/ln(10):0:2)
+else 
+writeln('4 функция не подходит по введенным значениям');
+end.
---
+++ 22222.pas
@@ -0,0 +1,17 @@
+﻿var x,form1,form2,form3,form4:real;
+begin
+x:= -10;
+while x <= 8 do
+begin 
+if x<-8 then 
+form1:=(cos(2*x)*((-1)*x+x**3)/cos(x));
+if (-8<=x) and (x<-5) then 
+form2:=59+ln(x);
+if (-5<=x) and (x<6) then 
+form3:=(-1)*x + tan(x);
+if 6<=x then
+form4:=ln(x) * ln(x)/ln(10);
+writeln('x1 = ', form1:0:2,' ','x2 = ', form2:0:2,' ','x3 = ', form3:0:2,' ','x4 = ', form4:0:2);
+x += 0.1;
+end;
+end.
